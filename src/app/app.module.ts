5cb6f2b21fd1911a3cfc596eff3e4b54b2d3d510
Ray Wagner <raywagner88@me.com>
2018-05-26 10:59:08 -0500

    Update app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { ListComponent } from './list/list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    ListComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
